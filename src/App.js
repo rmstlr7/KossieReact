b3dc430252fe8443ee917453832b94458de8b0ac
GeunSik Choi <rmstlr7@naver.com>
2025-12-06 22:48:01 +0900

    react 2장까지 commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function App() {
+  const [number, setNumber] = useState(1);
+  const double = () => {
+    setNumber((prevState) => prevState * 2);
+    setNumber((prevState) => prevState * 2);
+  };
+
+  return (
+    <>
+      <div>{number}</div>
+      <button className="btn btn-primary" onClick ={double}>Submit</button>
+    </>
+  );
+}
+
+export default App;
