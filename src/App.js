55a99dc0f3225b3a07e8d66bb0f31305efed7a72
GeunSik Choi <rmstlr7@naver.com>
2025-12-06 23:48:07 +0900

    [day3] 블로그 포스트 생성 폼 만들기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [number, setNumber] = useState(1);
-  const double = () => {
-    setNumber((prevState) => prevState * 2);
-    setNumber((prevState) => prevState * 2); 
-  };
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
-  return (
-    <>
-      <div>{number}</div>
-      <button className="btn btn-primary" onClick ={double}>Submit</button>
-    </>
-  );
+  const onSubmit = () => {
+    axios.post('http://localhost:3000/posts', {
+      title,
+      body
+    })
+  }
+
+  return(
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">title</label>
+        <input 
+          className="form-control" 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label className="form-label">body</label>
+        <textarea 
+          className="form-control"
+          rows="20"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+      <button 
+        className="btn btn-primary"
+        onClick={onSubmit}>
+        Post
+      </button>
+
+    </div>
+  )
 }
-
 export default App;
