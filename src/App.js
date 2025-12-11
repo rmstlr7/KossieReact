efb2c2a2bee56aea50ce2c0e36e1d34c640abf32
GeunSik Choi <rmstlr7@naver.com>
2025-12-11 22:51:12 +0900

    NavBar 컴포넌트로 빼내기

--- src/App.js
+++ src/App.js
@@ -2,37 +2,25 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from 'react-router-dom';
 import BlogForm from './components/BlogForm';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return(
     <Router>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Home</Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/blogs">
-                Blogs
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    
+      <NavBar />
+      <div className="container">
       <Switch>
-        <div className="container">
           <Route path="/" exact>
             Home page
           </Route>
           <Route path="/blogs">
             <BlogForm />
           </Route>
-        </div>
       </Switch>
+      </div>
     </Router>
   )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <ul className="navbar-nav">
+            <li className="nav-item">
+                <Link className="nav-link active" to="/blogs">
+                Blogs
+                </Link>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    )
+}
+export default NavBar;
