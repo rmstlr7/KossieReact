6c64dd3ba95fad8f256764865bdc0849eabf770b
GeunSik Choi <rmstlr7@naver.com>
2025-12-14 22:14:31 +0900

    34 생성 후 리스트 페이지로 이동

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router';
 const BlogForm = () => {
+
+    const history = useHistory();
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
@@ -9,6 +12,8 @@ const BlogForm = () => {
         axios.post('http://localhost:3001/posts', {
             title,
             body
+        }).then(() => {
+            history.push('/blogs');
         })
     }
 
